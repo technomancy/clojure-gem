7b31d92a19551aac030d2013c69fb732dee22095
Phil Hagelberg <technomancy@gmail.com>
2009-08-12 19:53:47 -0700

    Add Vector class.

--- lib/clojure/vector.rb
+++ lib/clojure/vector.rb
@@ -1,1 +1,30 @@
 require "clojure/clojure-1.0.0.jar"
+require "clojure/seq"
+
+module Clojure
+  Vector = Java::clojure::lang::PersistentVector
+
+  class Vector
+    include Seq
+
+    def self.new(array)
+      create(array)
+    end
+
+    def [](index, to = nil)
+      to, index = [index.first, index.last] if index.is_a? Range
+
+      if to # must be slicing
+        slice(index, to)
+      else
+        if index >= 0
+          RT.nth(self, index)
+        else
+          RT.nth(self, size + index)
+        end
+      end
+    end
+
+    # TODO: flatten, join, last, reverse, slice, uniq, to_a
+  end
+end
---
+++ test/test_vector.rb
@@ -0,0 +1,39 @@
+require File.dirname(__FILE__) + "/test_helper"
+
+class TestVector < Clojure::TestCase
+  include Clojure
+
+  def setup
+    @vector = Vector.new([1])
+    @vector2 = @vector.conj(2)
+    @vector3 = @vector2.conj(3)
+  end
+
+  def test_single_element
+    assert_equal 1, @vector.first
+    assert_equal 1, @vector.size
+    assert_nil @vector.next
+    assert @vector.rest.empty?
+    assert_equal "[1]", @vector.inspect
+  end
+
+  def test_multi_element
+    assert_equal 2, @vector2.next.first
+    assert_equal 1, @vector2.first
+    assert_equal 2, @vector2.size
+    assert_equal "[1 2]", @vector2.inspect
+  end
+
+  def test_lookup
+    assert_equal 3, @vector3[2]
+    assert_equal 3, @vector3[-1]
+  end
+
+  def test_map
+    assert_equal([2, 3, 4], @vector3.map {|x| x + 1})
+  end
+
+  def test_to_a
+    assert_equal [1, 2, 3], @vector3.to_a
+  end
+end
